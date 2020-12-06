891dd3bb7170a722aafc505050e01480a7fb9eb8
Karyna <karynabarkhatova@gmail.com>
2020-12-06 23:31:11 +0200

    Updated file main.py (with all tests in it)

--- main.py
+++ main.py
@@ -11,20 +11,26 @@ def parse_parameters(url):
 
 
 def parse_cookies(cookies):
-    if cookies == '':
+    count = 0
+    for i in cookies:
+        if not i == '=':
+            continue
+        else:
+            count += 1
+    if count == 0:
         return dict()
+    elif count == 1:
+        cookies_pairs = cookies.strip(';')
+        cookie = cookies_pairs.split('=')
+        key = cookie[0]
+        value = cookie[1]
+        d = dict()
+        d[key] = value
+        return d
     else:
-        try:
-            d = dict(x.split('=') for x in cookies.split(';'))
-            return d
-        except:
-            d = dict()
-            pairs = cookies.strip(';')
-            pair = pairs.split('=')
-            key = pair[0]
-            value = pair[1]
-            d[key] = value
-            return d
+        cookies2 = cookies.strip(';')
+        d = dict(x.split('=') for x in cookies2.split(';'))
+        return d
 
 
 if __name__ == '__main__':
@@ -40,7 +46,6 @@ if __name__ == '__main__':
     assert parse_parameters('https://www.google.com/search?q=cats&client=ubuntu&hs=Llg&sxsrf=ALeKk03r_grGYLuOdfHVpmW2qVuKJiAJzA:1607037631159&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjQ9JTZ-bLtAhXVFXcKHRtqCdIQ_AUoAXoECBEQAw&biw=1920&bih=944') == {'q': 'cats', 'client': 'ubuntu', 'hs': 'Llg', 'sxsrf': 'ALeKk03r_grGYLuOdfHVpmW2qVuKJiAJzA:1607037631159', 'source': 'lnms', 'tbm': 'isch', 'sa': 'X', 'ved': '2ahUKEwjQ9JTZ-bLtAhXVFXcKHRtqCdIQ_AUoAXoECBEQAw', 'biw': '1920', 'bih': '944'}
     assert parse_parameters('https://news.google.com/topstories?hl=ru&gl=UA&ceid=UA:ru') == {'hl': 'ru', 'gl': 'UA', 'ceid': 'UA:ru'}
 
-
     # Tests for function "parse_cookies"
     assert parse_cookies('') == {}
     assert parse_cookies('name=Dima;') == {'name': 'Dima'}
@@ -52,6 +57,3 @@ if __name__ == '__main__':
     assert parse_cookies('name=WMF-Last-Access;content=03-Dec-2020;path=/') == {'name': 'WMF-Last-Access', 'content': '03-Dec-2020', 'path': '/'}
     assert parse_cookies('name=_ga;content=GA1.2.1961323879.1607037432') == {'name': '_ga', 'content': 'GA1.2.1961323879.1607037432'}
     assert parse_cookies('name=_rxuuid;content=%7B%22rx_uuid%22%3A%22RX-95776ea7-297b-4710-871d-98c89147d635-003%22%7D') == {'name': '_rxuuid', 'content': '%7B%22rx_uuid%22%3A%22RX-95776ea7-297b-4710-871d-98c89147d635-003%22%7D'}
-    
-    
-    
